05f918ba6db18481d15bcca4b047dd6140bcc446
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-22 18:07:14 +0100

    Change argument type

--- include/binary.h
+++ include/binary.h
@@ -1,8 +1,10 @@
 #ifndef BINARY_H
 #define BINARY_H
 
+#include <iostream>
+
 namespace Binary {
-    long long int to_binary(int&);
+    long long int to_binary(int);
     int to_decimal(long long int&);
 }
 
--- src/binary.cpp
+++ src/binary.cpp
@@ -1,13 +1,13 @@
 #include "../include/binary.h"
 
-long long int Binary::to_binary(int& decimal)
+long long int Binary::to_binary(int decimal)
 {
     long long int binary {};
     int position {};
 
     while (decimal!=0)
     {
-        binary += decimal%2*position;
+        binary += (decimal%2)*position;
         decimal/=2;
         position*=10;
     }
