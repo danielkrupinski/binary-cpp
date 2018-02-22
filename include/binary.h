b77ff179b58fec47d685c9a425e0e125bbc123a1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-22 18:03:23 +0100

    Add functions and namespace

--- include/binary.h
+++ include/binary.h
@@ -1,4 +1,9 @@
 #ifndef BINARY_H
 #define BINARY_H
 
+namespace Binary {
+    long long int to_binary(int&);
+    int to_decimal(long long int&);
+}
+
 #endif BINARY_H
--- src/binary.cpp
+++ src/binary.cpp
@@ -1,1 +1,20 @@
 #include "../include/binary.h"
+
+long long int Binary::to_binary(int& decimal)
+{
+    long long int binary {};
+    int position {};
+
+    while (decimal!=0)
+    {
+        binary += decimal%2*position;
+        decimal/=2;
+        position*=10;
+    }
+    return binary;
+}
+
+int Binary::to_decimal(long long int& binary)
+{
+
+}
