bc9058c4975070fb0bc3ac4cabaae840a0ee32d9
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-22 18:31:25 +0100

    Add mode selection and number input

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+    std::cout << "Binary - decimal calculator\n";
+    std::cout << "Select mode:\n"
+              << "1. Binary to decimal\n"
+              << "2. Decimal to binary\n";
+    int mode {};
+    std::cin >> mode;
+    std::cout << "Enter a number: ";
+    long long int number {};
+    std::cin >> number;
+    if (mode == 1)
+        std::cout << to_binary(number) << '\n';
+    else
+        std::cout << to_decimal(number) << '\n';
     return 0;
 }
