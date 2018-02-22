d5dfcdbc11a22c498ab9a6d1573d37237e93788e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-22 18:38:37 +0100

    Source code optimination

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
 #include "../include/binary.h"
 
+using std::cout;
+
 int main()
 {
-    std::cout << "Binary - decimal calculator\n";
-    std::cout << "Select mode:\n"
-              << "1. Binary to decimal\n"
-              << "2. Decimal to binary\n";
+    cout << "Binary - decimal calculator\n"
+         << "Select mode:\n"
+         << "1. Binary to decimal\n"
+         << "2. Decimal to binary\n";
     int mode {};
     std::cin >> mode;
-    std::cout << "Enter a number: ";
+    cout << "Enter a number: ";
 
     if (mode == 1)
     {
         long long int number {};
         std::cin >> number;
-        std::cout << Binary::to_decimal(number) << '\n';
+        cout << Binary::to_decimal(number) << '\n';
     }
     else
     {
         int number {};
         std::cin >> number;
-        std::cout << Binary::to_binary(number) << '\n';
+        cout << Binary::to_binary(number) << '\n';
     }
     return 0;
 }
